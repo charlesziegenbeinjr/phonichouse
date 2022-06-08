2fbb384bccba8487c81a63588c7052112fb3e494
Ryan Chu <plochemisswag@gmail.com>
2022-06-07 21:45:48 -0700

    Update models.py

--- musicmarketplace/musicmarketplace_backend/musicmarketplace_backend/models.py
+++ musicmarketplace/musicmarketplace_backend/musicmarketplace_backend/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone_number = models.PositiveBigIntegerField()
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'username', 'password', 'phone_number']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'password', 'phone_number']
 
     
